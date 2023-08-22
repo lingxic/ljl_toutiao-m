e3087577388293ada3a47ec695aae864e6eed71e
lingxi <ljl1789555251@163.com>
2023-08-22 16:17:23 +0800

    项目初始化-引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
     <h1>黑马头条</h1>
+    <div>
+      <van-cell-group>
+        <van-cell title="单元格" value="内容"></van-cell>
+        <van-cell title="单元格" value="内容"></van-cell>
+      </van-cell-group>
+    </div>
     <router-view />
   </div>
 </template>
